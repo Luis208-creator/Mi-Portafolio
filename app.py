13185f77d658325f51fffc9865dd066cb35648ee
Luis208-creator <camarena.la@pucp.pe>
2024-09-29 00:25:59 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title("Luis Arturo Camarena")
+
+# Sección de Introducción
+st.subheader("Perfil")
+st.write("""
+Bachiller en Ciencias Sociales con mención en mención en Ciencia Política y Gobierno por la Pontificia Universidad Católica del Perú. Mis virtudes son un alto grado de compromiso con el equipo de trabajo, la vocación de servicio y la amabilidad. Encuentro muy interesante aprender y desarrollar conocimientos y habilidades nuevas. Me encuentro dispuesto a explorar diversos ámbitos profesionales
+""")
+
+# Sección de Información Personal
+st.subheader("Información Personal")
+st.write("""
+- **Email:** [camarena.la@pucp.pe]
+- **Ubicación:** [Lima, Perú]
+""")
+
+# Sección de Educación
+st.subheader("Cursos Relevantes")
+st.write("""
+- **[Bachiller en Ciencia Política y Gobierno]**, [PUCP], [2022]
+- **Diplomado en Ciencia de Datos**, [QLAB-PUCP], [Actualidad]
+""")
+# Cursos Relevantes
+st.subheader("Educación")
+st.write("""
+- **[Innovación en el Sector Público]**, [Escuela de Gobierno y Políticas Públicas PUCP], [2020]
+- **Herramientas Estadísticas para el Análisis Sociopolítico**, [CISEPA PUCP]], [2017]
+""")
+
+# Voluntariados
+st.subheader("Voluntariados")
+st.write("""
+- **[Innovación en el Sector Público]**, [Escuela de Gobierno y Políticas Públicas PUCP], [2020]
+- **Herramientas Estadísticas para el Análisis Sociopolítico**, [CISEPA PUCP]], [2017]
+""")
+
+# Sección de Experiencia Laboral
+st.subheader("Experiencia Laboral")
+st.write("""
+- **Banco de alimentos del Perú ]**, [ Voluntario] 
+  - [Voluntario encargado de la supervisión y rescate de alimentos en los supermercados Plaza Vea y Wong..]
+
+- **[Asociación Interdisciplinaria Peruana de Modernización del Estado]**, [2020] 
+  - - Voluntario encargado de realizar entrevistas para el rediseño del Programa Presupuestal 0062.]
+""")
+
+# Sección de Habilidades
+st.subheader("Habilidades")
+st.write("""
+- Vocación de Servicio
+- Análisis de Datos
+- Pensamiento Crítico
+- Trabajo en equipo
+- Inglés Avanzado-Certificado
+
+""")
+
+
+
+# Sección de Contacto
+st.subheader("Contacto")
+st.write("""
+Si deseas contactarme, por favor envíame un correo a [camarena.la@pucp.pe] 
+""")
+
